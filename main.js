a52ecb9dfb3188412e1400960deaeb86a97617d5
stulo <sterlingfairfield@gmail.com>
2021-08-22 14:07:58 -0700

    updating logging

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
+// TODO: add user to input test cases
+
 // check-for-even-or-odd.js
 const parity = require('./js-modules/check-for-even-or-odd.js')
 const numString = "1, 3, 6, 7, 9"
 
-console.log(parity(numString))
+console.info('\x1b[32m%s\x1b[0m', 'Check for even or odd numbers')
+console.log(`String: ${numString}. Position of even or odd: ${parity(numString)}`)
+
+console.log('---------------------------------------------------------')
 
 // reverse-array.js
 const reverse = require('./js-modules/reverse-array.js')
 const num = 123;
 
-console.log(reverse(num))
+console.info('\x1b[32m%s\x1b[0m', 'Reversed array')
+console.log(`Integer: ${num}. Reversed in array: ${JSON.stringify(reverse(num))}`)
