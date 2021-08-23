4e218587f80563a1f3b69e242992abe606a25d0d
sfairfield <sterling.fairfield@lularoe.com>
2021-08-23 13:12:14 -0700

    changing double quotes to single quotes

--- reverse-array/main.js
+++ reverse-array/main.js
@@ -1,7 +1,7 @@
-const reverse = require("./reverse-array.js");
+const reverse = require('./reverse-array.js');
 const num = 123;
 
-console.info("\x1b[32m%s\x1b[0m", "Reversed array"); // green text
+console.info('\x1b[32m%s\x1b[0m', 'Reversed array'); // green text
 console.log(
   `Integer: ${num}. Reversed in array: ${JSON.stringify(reverse(num))}`
 );
